4039702665ad90f7cbc6dda5d6ed0c6907ebe385
NIKU-SINGH <soapmactavishmw4@gmail.com>
2023-11-24 15:41:59 +0530

    patch: added elasticsearch.yml

--- services/logConsumer/consumer.ts
+++ services/logConsumer/consumer.ts
@@ -24,7 +24,7 @@ export async function Consumer() {
             console.error("Error parsing message:", error);
           }
         } else {
-          console.warn("Message has no value");
+          console.warn("Message has no value"); 
         }
       },
     });
